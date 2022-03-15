e33918081a4cc2baffbfce7e516c25617ab3da6b
BradlyKim <kjh5864@naver.com>
2022-03-15 14:16:10 +0900

    어댑터 getView 함수 기초 작업

--- app/src/main/java/com/neppplus/orderapp_20220306/adapters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.orderapp_20220306.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.orderapp_20220306.R
 import com.neppplus.orderapp_20220306.datas.StoreData
 
 class StoreAdapter(
@@ -10,6 +14,16 @@ class StoreAdapter(
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
 
+        val row = tempRow!!
+
+        return row
+
+    }
 
 }
