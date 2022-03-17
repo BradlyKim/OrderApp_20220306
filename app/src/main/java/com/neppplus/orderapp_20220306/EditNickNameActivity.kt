efb2ebabb44d0f421bd1117101337f93b485f09b
BradlyKim <kjh5864@naver.com>
2022-03-17 18:36:51 +0900

    닉네임을 입력하고 돌아오면 > 프래그먼트에서 받아서 적용

--- app/src/main/java/com/neppplus/orderapp_20220306/EditNickNameActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220306/EditNickNameActivity.kt
@@ -1,11 +1,33 @@
 package com.neppplus.orderapp_20220306
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+        btnSave.setOnClickListener {
+
+            val inputNickname = edtNickName.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+        }
+
+    }
+
+    fun setValues() {
+
     }
 }
